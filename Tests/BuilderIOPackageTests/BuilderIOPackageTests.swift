38af0a1618083dcc3472c59688228b68563f9908
Krishna Vivek <vivek4srirama@yahoo.com>
2022-10-21 15:14:52 +0100

    Initial Commit

---
+++ Tests/BuilderIOPackageTests/BuilderIOPackageTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import BuilderIOPackage
+
+final class BuilderIOPackageTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(BuilderIOPackage().text, "Hello, World!")
+    }
+}
